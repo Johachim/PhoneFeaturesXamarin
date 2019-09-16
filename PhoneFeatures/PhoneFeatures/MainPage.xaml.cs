b021e7e75362d76eefcdea0062c0cec8c61b0763
Johachim <joachimgul@yahoo.dk>
2019-09-16 13:04:27 +0200

    battery og shake

---
+++ PhoneFeatures/PhoneFeatures/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace PhoneFeatures
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SetBackground(Battery.ChargeLevel, Battery.State == BatteryState.Charging);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            Battery.BatteryInfoChanged += Battery_BatteryInfoChanged;
+
+            Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
+            Accelerometer.Start(SensorSpeed.Game);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Battery.BatteryInfoChanged -= Battery_BatteryInfoChanged;
+
+            Accelerometer.ShakeDetected -= Accelerometer_ShakeDetected;
+            Accelerometer.Stop();
+        }
+
+        private void Accelerometer_ShakeDetected(object sender, EventArgs e)
+        {
+            DisplayAlert("HEY", "I'm shook", "sorry");
+        }
+
+        private void Battery_BatteryInfoChanged(object sender, BatteryInfoChangedEventArgs e)
+        {
+            SetBackground(e.ChargeLevel, e.State == BatteryState.Charging);
+        }
+        void SetBackground(double level, bool charging)
+        {
+            Color? color = null;
+            var status = charging ? "Charging" : "Not charging";
+
+            if (level > .5f)
+            {
+                color = Color.Green.MultiplyAlpha(level);
+            }
+            else if (level > .1f)
+            {
+                color = Color.Yellow.MultiplyAlpha(1d - level);
+            }
+            else
+            {
+                color = Color.Red.MultiplyAlpha(1d - level);
+            }
+
+            BackgroundColor = color.Value;
+            LabelBatteryLevel.Text = status;
+
+        }
+
+    }
+}
